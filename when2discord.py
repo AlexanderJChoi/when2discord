2cb9bbd48cfa4a5c16ca260090b49d78037520ae
Alexander Choi <32470094+AlexanderJChoi@users.noreply.github.com>
2023-12-14 21:48:38 -0700

    Fixed bot config to load commands correctly

--- when2discord.py
+++ when2discord.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 # Load Environment variables
@@ -8,17 +9,25 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 TESTING_GUILD_ID = os.getenv('TESTING_GUILD_ID')
 
-# Initialize bot and guild
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='w2d!', description='A bot that will help you schedule events!', intents=intents)
 my_guild = discord.Object(id=TESTING_GUILD_ID)
+
+# Define Bot
+class MyBot(commands.Bot):
+	async def setup_hook(self):
+		self.tree.copy_global_to(guild=my_guild)
+		await self.tree.sync(guild=my_guild)
+
+# Initialize bot
+intents = discord.Intents.default()
+bot = MyBot(command_prefix='w2d!', description='A bot that will help you schedule events!', intents=intents)
 
 # Implement helper functions
 
 # Create and implement commands here
-@bot.tree.command(name="hello_world", description="this is a test slash command", guilds=[my_guild])
-async def hello_world(ctx):
-	await ctx.send("HELLO.  :)")
+@bot.tree.command()
+@app_commands.describe(words="The words you input")
+async def hello_world(interaction: discord.Interaction, words: str = ""):
+	await interaction.response.send_message("HELLO.  :)" + words, ephemeral=True, delete_after=30)
 
 # Start bot
 bot.run(TOKEN)
