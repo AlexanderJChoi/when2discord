df2f1b0404376301065af19588b1d187e3b03f2d
Alexander Choi <32470094+AlexanderJChoi@users.noreply.github.com>
2023-12-31 09:29:54 -0700

    Add create_event command. Gets event title, guild_id, earliest start and latest end times.

--- when2discord.py
+++ when2discord.py
@@ -4,6 +4,8 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
+from datetime import datetime, time
+
 # Import local modules
 import sys
 sys.path.append(".")
@@ -28,11 +30,41 @@ bot = MyBot(command_prefix='w2d!', description='A bot that will help you schedul
 
 # Implement helper functions
 
+# Implement Data Transformers
+class TimeTransformer(app_commands.Transformer):
+	async def transform(self, interaction: discord.Interaction, value: str) -> time:
+		tv = datetime.strptime(value, "%I:%M%p")
+		return tv.time()
+
+# Handle any errors
+@bot.tree.error
+async def on_command_error(interaction: discord.Interaction, error):
+	if isinstance(error, app_commands.TransformerError):
+		await interaction.response.send_message(f"COMMAND {interaction.command.name} FAILED. COULD NOT TRANSFORM VALUE {error.value}. INCORRECT FORMAT.", ephemeral=True, delete_after=30)
+
 # Create and implement commands here
 @bot.tree.command()
 @app_commands.describe(words="The words you input")
 async def hello_world(interaction: discord.Interaction, words: str = ""):
 	await interaction.response.send_message("HELLO.  :)" + words, ephemeral=True, delete_after=30)
+	
+# TODO: write better description of expected time format
+@bot.tree.command()
+@app_commands.describe(name="The name of this event", 
+	no_earlier_than_time="The earliest time the event might begin (format like: 10:31PM or 10:31pm)", 
+	no_later_than_time="The latest time the event might end (format like: 10:31PM or 10:31pm)",
+	timezone="The event's local timezone. This parameter does nothing in the current version.")
+@app_commands.rename(no_earlier_than_time="no_earlier_than",
+	no_later_than_time="no_later_than")
+async def create_event(interaction: discord.Interaction, name: str, no_earlier_than_time: app_commands.Transform[time, TimeTransformer], no_later_than_time: app_commands.Transform[time, TimeTransformer], timezone: str or None=None):
+	group_id = interaction.guild_id
+	message = f"{name} : {str(no_earlier_than_time)} : {str(no_later_than_time)} : {str(group_id)}"
+	ephemeral = False
+	if no_later_than_time < no_earlier_than_time:
+		message="ERROR no_later_than must be after no_earlier_than"
+		ephemeral=True
+	# TODO: create a View for adding date intervals
+	await interaction.response.send_message(message, ephemeral=ephemeral)
 
 # Testing Switching_View
 @bot.tree.command()
